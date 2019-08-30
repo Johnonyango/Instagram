b930416b0347da1e5b673b567ff986dfd51d447d
Johnonyango <j.yayah7@gmail.com>
2019-08-30 19:01:58 +0300

    create urls.py file in instagram

---
+++ instagram/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+
+
+
+
+urlpatterns=[
+    url('^$',views.login_page,name = 'come'),
+]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def welcome(request):
+    current_user =request.user
+    posts = Image.images_all()
+    profile = Profile.objects.get(username=current_user)
+    users = Profile.objects.all()
+    to_follow = User.objects.all().exclude(id=request.user.id)
+    return render(request, 'index.html', {"posts":posts,"profile":profile, "users":users, "views":to_follow}
