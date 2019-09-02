57c9d6240ae705187d3af313b10f581ee18ec467
Johnonyango <j.yayah7@gmail.com>
2019-09-02 10:39:00 +0300

    make migrations

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.login_page,name = 'come'),
-    url('^home$',views.welcome,name = 'welcome'),
-    url('^search/', views.search_results, name = 'search_results'),
+    # url(r'^$',views.login_page,name = 'come'),
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^new/article$', views.new_post, name='new-article'),
     url(r'^new/profile$', views.new_profile, name='new-profile'),
-    url(r'^profile/$', views.profile, name='profile'),
-    url(r'^profile/user/(\d+)$', views.view_users, name='users')
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^profile/user/(\d+)', views.view_users, name='users'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
